644cf15a9e70ee3262d64e48b7a2e68ae40fa320
Rohit T <rohit.tagadiya@gmail.com>
2025-07-11 15:13:31 +0100

    update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.svg" />
+        
+        {/* Theme color for mobile browsers */}
+        <meta name="theme-color" content="#059669" />
+        
+        {/* Manifest for PWA */}
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
